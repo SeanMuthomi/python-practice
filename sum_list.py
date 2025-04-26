851f5b4e6e26ecc46e615fa3f943717f482325d5
SeanMuthomi <sean.muthomi@strathmore.edu>
2025-04-26 20:56:32 +0300

    Update sum_list.py

--- sum_list.py
+++ sum_list.py
@@ -1,7 +1,7 @@
 def sum_list(numbers):
- total=0
-for num in numbers:
-total += num
+ total=0 #first number is zero
+for num in numbers: #loop the digits
+total += num #add to total
 return total
 
 my_list ={1,2,3,4,5}
