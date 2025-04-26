bddbc8fd7a9402411aba947f30ef9bf1bac521ba
SeanMuthomi <sean.muthomi@strathmore.edu>
2025-04-26 20:59:34 +0300

    Update factorial_loop.py

--- factorial_loop.py
+++ factorial_loop.py
@@ -1,7 +1,7 @@
 def factorial_loop(n):
- result=1
-for I in range(1,n+1):
- result *=I
-return result
+ result=1 #start with 1
+for I in range(1,n+1): #loop to be continuous
+ result *=I#multiply the result with i
+return result #return final value
 
 print(factorial loop(10))
